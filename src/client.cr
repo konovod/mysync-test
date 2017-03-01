d157351ab103203f1beea1acdcaa04bb5d363554
Konovod <konovod@gmail.com>
2017-02-28 23:37:17 -0100

    fixed typos

--- src/client.cr
+++ src/client.cr
@@ -21,7 +21,7 @@ class TestClientEndpoint < MySync::EndPoint(TestClientInput, TestServerOutput)
   end
 
   def before_sending_sync
-    p "sending"
+    # p "sending"
   end
 
   solve_bug
@@ -30,13 +30,13 @@ end
 cli = TestClientEndpoint.new
 udp_cli = MySync::UDPGameClient.new(cli, Socket::IPAddress.new(ARGV[0], 12000))
 public = Crypto::PublicKey.new(secret: Crypto::SecretKey.new("c4b12631c3f68e7a72fc760a31ffaae0a7a5f8d892cac43c0d8d06acd1b3fd8e"))
-p udp_cli.login(public_key, "it_s_me".to_slice)
+p udp_cli.login(public, "it_s_me".to_slice)
 
 cur = Time.now
 cli.benchmark = 10000
 cli.benchmark_udp = udp_cli
 udp_cli.send_data
-cli.benchmark_chan.receive
+cli.benchmark_complete.receive
 pp (Time.now - cur).to_f/10.0 # *1000 / 10000
 pp cli.stat_losses
 pp cli.stat_pingtime*1000
